590b8a2fd1e5dae972d19cdeb9e2abe8b74730c7
Odin Rochmman <odin.rochmann@gmail.com>
2019-02-06 20:17:12 +0100

    greenkeep

--- packages/plugin-lit-html-svg/lib/template.d.ts
+++
@@ -1,5 +0,0 @@
-export interface Template {
-    (css: string): string;
-}
-export declare const template: Template;
-export default template;
--- packages/plugin-lit-html-svg/lib/template.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.template = (css) => `
-  import { svg } from "lit-element";
-  export const symbol = svg\`${css}\`;
-  export default symbol;
-`;
-exports.default = exports.template;
